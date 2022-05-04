8e42c7b6d5c0a82bd1883835a50e559ef7386dce
Mayerstrk <mayerstrk@gmail.com>
2022-05-04 21:46:52 +0300

    third attempt

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,29 +1,28 @@
-const MODAL = document.querySelector(".modal__page-overlay");
+const MODAL = document.querySelector(".modal");
 const FORM = document.querySelector(".form");
 const EDIT_BTN = document.querySelector(".profile__edit-button")
 const CLOSE_BTN = MODAL.querySelector(".modal__close-button");
-const SAVE_BTN = FORM.querySelector(".form__button");
 const PROFILE_NAME = document.querySelector(".profile__name")
 const PROFILE_CAPTION = document.querySelector(".profile__caption")
-let inputName = FORM.querySelector(".form__input_type_name");
-let inputCaption = FORM.querySelector(".form__input_type_caption");
+const INPUT_NAME = FORM.querySelector(".form__input_type_name");
+const INPUT_CAPTION = FORM.querySelector(".form__input_type_caption");
 
 // loads user's profile information into corresponding input values
 function renderProfileInfo() {
-  inputName.value = PROFILE_NAME.textContent
-  inputCaption.value = PROFILE_CAPTION.textContent
+  INPUT_NAME.value = PROFILE_NAME.textContent
+  INPUT_CAPTION.value = PROFILE_CAPTION.textContent
 }
 
 // Toggles modal window visibility and
 // execues renderProfileInfo
 function openEditModal() {
-  MODAL.classList.toggle("modal__page-overlay_visible");
+  MODAL.classList.toggle("modal_visible");
   renderProfileInfo();
 }
 
 // Toggles modal window visibility
 function closeEditModal() {
-  MODAL.classList.toggle("modal__page-overlay_visible");
+  MODAL.classList.toggle("modal_visible");
 }
 
 
@@ -33,13 +32,13 @@ function handleProfileFormSubmit(evt) {
   evt.preventDefault(); 
 
   // Applies changes 
-  PROFILE_NAME.textContent = `${inputName.value}`;
-  PROFILE_CAPTION.textContent = `${inputCaption.value}`;
+  PROFILE_NAME.textContent = `${INPUT_NAME.value}`;
+  PROFILE_CAPTION.textContent = `${INPUT_CAPTION.value}`;
 
   // Sets value of input field's placeholder attribute to
   // current state
-  inputName.placeholder = `${inputName.value}`;
-  inputCaption.placeholder = `${inputCaption.value}`;
+  INPUT_NAME.placeholder = `${INPUT_NAME.value}`;
+  INPUT_CAPTION.placeholder = `${INPUT_CAPTION.value}`;
 
   //Closes modal window
   closeEditModal();
