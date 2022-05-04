9f0adf57eb8eed909052023d7c9a9346840aa8b2
Mayerstrk <mayerstrk@gmail.com>
2022-05-04 10:09:41 +0300

    second attempt

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,16 +1,31 @@
-const MODAL = document.querySelector(".modal-page-overlay");
+const MODAL = document.querySelector(".modal__page-overlay");
 const FORM = document.querySelector(".form");
 const EDIT_BTN = document.querySelector(".profile__edit-button")
 const CLOSE_BTN = MODAL.querySelector(".modal__close-button");
 const SAVE_BTN = FORM.querySelector(".form__button");
 const PROFILE_NAME = document.querySelector(".profile__name")
 const PROFILE_CAPTION = document.querySelector(".profile__caption")
+let inputName = FORM.querySelector(".form__input_type_name");
+let inputCaption = FORM.querySelector(".form__input_type_caption");
 
-
-//Toggles modal window visibility
-function toggleEditModal() {
-  MODAL.classList.toggle("modal-page-overlay_visible");
+function renderProfileInfo() {
+  // loads user's profile information into corresponding input values
+  inputName.value = PROFILE_NAME.textContent
+  inputCaption.value = PROFILE_CAPTION.textContent
+  
 }
+// Toggles modal window visibility and
+// execues renderProfileInfo
+function openEditModal() {
+  MODAL.classList.toggle("modal__page-overlay_visible");
+  renderProfileInfo();
+}
+
+// Toggles modal window visibility
+function closeEditModal() {
+  MODAL.classList.toggle("modal__page-overlay_visible");
+}
+
 
 
 function handleProfileFormSubmit(evt) {
@@ -18,30 +33,24 @@ function handleProfileFormSubmit(evt) {
   //Stops browser from submitting form in the default way
   evt.preventDefault(); 
 
-  let inputName = FORM.querySelector(".form__input_type_name");
-  let inputCaption = FORM.querySelector(".form__input_type_caption");
-
   // Applies changes 
   PROFILE_NAME.textContent = `${inputName.value}`;
   PROFILE_CAPTION.textContent = `${inputCaption.value}`;
 
-  // 1. Sets value of input field's placeholder attribute to
-  //    current state
+  // Sets value of input field's placeholder attribute to
+  // current state
   inputName.placeholder = `${inputName.value}`;
   inputCaption.placeholder = `${inputCaption.value}`;
 
-  // 2. Resets value of form input fields 
-  inputName.value = "";
-  inputCaption.value = "";
-
   //Closes modal window
-  toggleEditModal();
+  closeEditModal();
 }
 
 
 // Toggles modal windo visibility when buttons are clicked
-EDIT_BTN.addEventListener("click", toggleEditModal);
-CLOSE_BTN.addEventListener("click", toggleEditModal);
+// and loads profile info on modal open
+EDIT_BTN.addEventListener("click", openEditModal);
+CLOSE_BTN.addEventListener("click", closeEditModal);
 
 // Saves changes after form is filled and submitted
-SAVE_BTN.addEventListener("click", handleProfileFormSubmit);
+FORM.addEventListener("submit", handleProfileFormSubmit);
