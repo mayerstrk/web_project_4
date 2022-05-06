9f9d31634450956dffaa809b5f081c1c940b54f7
Mayerstrk <mayerstrk@gmail.com>
2022-05-06 18:32:11 +0300

    first attempt

--- scripts/modal_type_card.js
+++ scripts/modal_type_card.js
@@ -3,19 +3,19 @@ const CARD_MODAL = document.querySelector(".modal_type_photo");
 
 
 // 1. Sets the values for the card modal depending on which picture is clicked
-// 2. Toggles modal visibility
+// 2. Toggles modal window visibility
 const OPEN_CARD_MODAL = evt => {
   // 1.
-  // Gets relevant card values for the modal
+  // Gets relevant values from the card's elements to assign them to the modal window's elements
   const SRC_VALUE = evt.target.closest(".card__photo").getAttribute("src");
   const CAPTION_TEXT = evt.target.parentElement.nextElementSibling.firstElementChild.textContent;
 
-  // Assigns values to corresponding elements
+  // Assigns the values to their corresponding elements in the modal window
   CARD_MODAL.querySelector(".card-modal__photo").setAttribute("src", SRC_VALUE);
   CARD_MODAL.querySelector(".card-modal__photo-caption").textContent = CAPTION_TEXT;
 
   // 2.
-  // Toggles modal visibility
+  // Toggles modal window visibility
   CARD_MODAL.classList.add("modal_visible");
 }
 
