bfabd4d633799cf7888a3e1735f35dc69802e121
Mayerstrk <mayerstrk@gmail.com>
2022-05-07 03:03:12 +0300

    third attempt

--- scripts/modal_type_card.js
+++ scripts/modal_type_card.js
@@ -1,8 +1,12 @@
 const cardModal = document.querySelector(".modal_type_card");
 
+// Queries the card modal close button and adds the event listener
+const cardModalCloseButton = cardModal.querySelector(".modal__close-button_for_card-modal");
+cardModalCloseButton.addEventListener("click", () => closeModal(cardModal));
+
 
 // Gets the values for the card modal depending on which picture is clicked
-function renderCardModalValues(evt) {
+function getCardModalValues(evt) {
   // Gets relevant values from the card's elements to assign them to the modal window's elements
   const srcValue = evt.target.closest(".card__photo").getAttribute("src");
   const captionText = evt.target.parentElement.nextElementSibling.firstElementChild.textContent;
@@ -20,12 +24,7 @@ function renderCardModalValues(evt) {
 // Renders the card modal window with the appropiate values
 const renderCardModal = evt => {
   // Gets appropiate values
-  renderCardModalValues(evt)
+  getCardModalValues(evt)
   // Calls the openModal function from modal_functions.js to toggle modal window visibility
   openModal(cardModal)
 };
-
-
-// Queries the card modal close button and adds the event listener
-const cardModalCloseButton = cardModal.querySelector(".modal__close-button_for_card-modal");
-cardModalCloseButton.addEventListener("click", () => closeModal(cardModal));
